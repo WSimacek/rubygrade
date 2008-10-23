ae5316044e2e50f86e948a8bab7368939f9fc4b4
Ariejan de Vroom <ariejan@ariejan.net>
2008-10-23 13:01:45 +0200

    Installed configatron 2.1.5 gem

--- config/environment.rb
+++ config/environment.rb
@@ -13,6 +13,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   # Gems
   config.gem "capistrano-ext", :lib => "capistrano"
+  config.gem "configatron"
 
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
