a067f7a028a34efc75c9a2bd442a6026cfebc608
Leandro de Oliveira <ol.leandro@gmail.com>
2008-12-07 07:48:51 +0800

    Rails 2.2 compatibility fixes
    Signed-off-by: Ariejan de Vroom <ariejan@ariejan.net>

--- config/environment.rb
+++ config/environment.rb
@@ -5,20 +5,25 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # Gems
-  config.gem "capistrano-ext", :lib => "capistrano"
+  #config.gem "capistrano-ext", :lib => "capistrano"
   config.gem "configatron"
 
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   config.time_zone = 'UTC'
   
+  # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
+  # All files from config/locales/*.rb,yml are added automatically.
+  # config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
+  # config.i18n.default_locale = :de
+
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
   # Make sure the secret is at least 30 characters and all random, 
